c12d3b1f2f41b33d499e4deaf780d8065ceabf7b
Gand0rf <gand0rf@protonmail.com>
2025-08-01 21:55:15 -0400

    small update

--- scan_tools/html_scan.py
+++ scan_tools/html_scan.py
@@ -8,10 +8,11 @@ def basic_scan(host,switches):
     scan_results = Popen([command],stdout=PIPE,stderr=PIPE,shell=True)
     scan_output, scan_error = scan_results.communicate()
     if scan_output:
+        print('\n Output of directory scan:')
         print(scan_output.decode('utf-8'))
     else:
+        print('\nError when attempting scan:')
         print(scan_error.decode('utf-8'))
 
 if __name__ == ("__main__"):
-    basic_scan('127.0.0.1','-fc 403')
     exit
